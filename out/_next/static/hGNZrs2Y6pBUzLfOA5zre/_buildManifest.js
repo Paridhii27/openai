45c21bc11789f7e36ed604520810c2b2c43f88b6
Paridhi <paridhigarg27@gmail.com>
2023-04-28 15:44:18 -0400

    npm export

---
+++ out/_next/static/hGNZrs2Y6pBUzLfOA5zre/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/602-c9e2d9a284ee12ca.js","static/chunks/pages/index-4518a9ac1e80101d.js"],"/_error":["static/chunks/pages/_error-dfcfa5bb62767c20.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
